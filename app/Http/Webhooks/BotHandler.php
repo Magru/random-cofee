4c7eeb49564255ec3e2df8c0599fee0a366a5742
Maxim Folko <maxf@leos.co.il>
2022-07-26 11:40:27 +0300

    bot

--- app/Http/Webhooks/BotHandler.php
+++ app/Http/Webhooks/BotHandler.php
@@ -3,11 +3,21 @@
 namespace App\Http\Webhooks;
 
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Models\TelegraphBot;
+use DefStudio\Telegraph\Models\TelegraphChat;
 
 class BotHandler extends WebhookHandler
 {
     public function start(): void
     {
+
+
+        $this->chats()->create([
+            'chat_id' => $this->chat->chat_id,
+            'name' => 'Hello',
+        ]);
+
+
         $this->chat->markdown("*Hi* happy to be here!")->send();
     }
 }
