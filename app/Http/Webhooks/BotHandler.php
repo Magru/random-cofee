41c427b853b736abcee4bb22b44f790bacb3ba46
Maxim Folko <maxf@leos.co.il>
2022-07-26 11:55:37 +0300

    bot

--- app/Http/Webhooks/BotHandler.php
+++ app/Http/Webhooks/BotHandler.php
@@ -13,6 +13,6 @@ class BotHandler extends WebhookHandler
         $chat_id = $this->chat->chat_id;
         $bot = TelegraphBot::fromId(1);
 
-        $this->chat->markdown('Chat ID: ' . $chat_id . 'Bot ID: ' . print_r($bot->info(), true))->send();
+        $this->chat->markdown('Hello')->send();
     }
 }
