b441e6c6b7b214d416bc8c78f4d4b4823144d693
Maxim Folko <maxf@leos.co.il>
2022-07-28 16:58:21 +0300

    bot tests

--- app/Http/Webhooks/BotHandler.php
+++ app/Http/Webhooks/BotHandler.php
@@ -3,6 +3,8 @@
 namespace App\Http\Webhooks;
 
 use App\Models\User;
+use DefStudio\Telegraph\DTO\InlineQuery;
+use DefStudio\Telegraph\DTO\InlineQueryResultPhoto;
 use DefStudio\Telegraph\Enums\ChatActions;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 use DefStudio\Telegraph\Keyboard\Button;
@@ -12,19 +14,34 @@ use DefStudio\Telegraph\Keyboard\ReplyKeyboard;
 use DefStudio\Telegraph\Models\TelegraphBot;
 use DefStudio\Telegraph\Models\TelegraphChat;
 use DefStudio\Telegraph\Telegraph;
+use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Log;
+
 
 class BotHandler extends WebhookHandler
 {
     public function start(): void
     {
-        $response = Telegram::getWebhookDebugInfo()->send();
-        Log::info($response);
+        $this->chat->message('hello world')
+            ->keyboard(Keyboard::make()->buttons([
+                Button::make('Delete')->action('handleInlineQuery')->param('id', '42'),
+                Button::make('open')->url('https://test.it'),
+                Button::make('Web App')->webApp('https://web-app.test.it'),
+            ]))->send();
     }
 
-    public function delete(){
-        Log::info('delete action');
+        public function handleInlineQuery(InlineQuery $inlineQuery): void
+    {
+        $query = $inlineQuery->query(); // "pest logo"
 
+        $logo = 'aaa'; // the code to handle the query. just an example here
+
+        $this->bot->answerInlineQuery($inlineQuery->id(), [
+            InlineQueryResultPhoto::make($logo."-light", "https://logofinder.dev/$logo/light.jpg", "https://logofinder.dev/$logo/light/thumb.jpg")
+                ->caption('Light Logo'),
+            InlineQueryResultPhoto::make($logo."-dark", "https://logofinder.dev/$logo->id/dark.jpg", "https://logofinder.dev/$logo/dark/thumb.jpg")
+                ->caption('Light Logo'),
+        ])->send();
     }
 
 
