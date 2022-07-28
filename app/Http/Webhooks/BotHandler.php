5dfe95155a837fab9f89a5d65e5be7e8a9e2e22b
Maxim Folko <maxf@leos.co.il>
2022-07-28 22:22:07 +0300

    bot tests

--- app/Http/Webhooks/BotHandler.php
+++ app/Http/Webhooks/BotHandler.php
@@ -3,18 +3,9 @@
 namespace App\Http\Webhooks;
 
 use App\Models\User;
-use DefStudio\Telegraph\DTO\InlineQuery;
-use DefStudio\Telegraph\DTO\InlineQueryResultPhoto;
-use DefStudio\Telegraph\Enums\ChatActions;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 use DefStudio\Telegraph\Keyboard\Button;
 use DefStudio\Telegraph\Keyboard\Keyboard;
-use DefStudio\Telegraph\Keyboard\ReplyButton;
-use DefStudio\Telegraph\Keyboard\ReplyKeyboard;
-use DefStudio\Telegraph\Models\TelegraphBot;
-use DefStudio\Telegraph\Models\TelegraphChat;
-use DefStudio\Telegraph\Telegraph;
-use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Log;
 
 
@@ -26,10 +17,17 @@ class BotHandler extends WebhookHandler
 
         $this->chat->message('hello world')
             ->keyboard(Keyboard::make()->buttons([
-                Button::make('Delete')->action('handleInlineQuery')->param('id', '42'),
+                Button::make('Начнем ?')->action('dismiss')->param('id', '455552'),
                 Button::make('open')->url('https://test.it'),
                 Button::make('Web App')->webApp('https://web-app.test.it'),
             ]))->send();
+    }
+
+    public function dismiss(){
+
+        $key1 = $this->data->get('id');
+        Log::debug($key1);
+
     }
 
 }
