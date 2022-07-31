9d20e4d5652a5f9bcec050273539d828eeeef532
Maxim Folko <maxf@leos.co.il>
2022-07-31 14:54:09 +0300

    bot tests

--- routes/telegram.php
+++ routes/telegram.php
@@ -4,6 +4,7 @@
 use SergiX44\Nutgram\Conversations\InlineMenu;
 use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
+use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardMarkup;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,7 +18,7 @@ use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
 
 
 
-$bot->onCommand('example', ChooseColorMenu::class);
+$bot->onCommand('example', AskIceCreamConversation::class);
 
 
 class ChooseColorMenu extends InlineMenu
@@ -43,6 +44,44 @@ class ChooseColorMenu extends InlineMenu
     public function none(Nutgram $bot)
     {
         $bot->sendMessage('Bye!');
+        $this->end();
+    }
+}
+
+class AskIceCreamConversation extends Conversation {
+
+    protected ?string $step = 'askCupSize';
+
+    public $cupSize;
+
+    public function askCupSize(Nutgram $bot)
+    {
+        $bot->sendMessage('How big should be you ice cream cup?', [
+            'reply_markup' => InlineKeyboardMarkup::make()
+                ->addRow(InlineKeyboardButton::make('Small', callback_data: 'S'), InlineKeyboardButton::make('Medium', callback_data: 'M'))
+                ->addRow(InlineKeyboardButton::make('Big', callback_data: 'L'), InlineKeyboardButton::make('Super Big', callback_data: 'XL')),
+        ]);
+        $this->next('askFlavors');
+    }
+
+    public function askFlavors(Nutgram $bot)
+    {
+        // if is not a callback query, ask again!
+        if (!$bot->isCallbackQuery()) {
+            $this->askCupSize($bot);
+            return;
+        }
+
+        $this->cupSize = $bot->callbackQuery()->data;
+
+        $bot->sendMessage('What flavors do you like?');
+        $this->next('recap');
+    }
+
+    public function recap(Nutgram $bot)
+    {
+        $flavors = $bot->message()->text;
+        $bot->sendMessage("You want an $this->cupSize cup with this flavors: $flavors");
         $this->end();
     }
 }
