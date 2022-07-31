277d9708b9448fa94904e92664329d475ddf00ec
Maxim Folko <maxf@leos.co.il>
2022-07-31 13:14:22 +0300

    bot tests

--- app/Http/Controllers/TelegramBot/BotTestController.php
+++ app/Http/Controllers/TelegramBot/BotTestController.php
@@ -7,38 +7,15 @@ use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardMarkup;
 
 class AskIceCreamConversation extends Conversation {
 
-    protected ?string $step = 'askCupSize';
-
-    public $cupSize;
-
-    public function askCupSize(Nutgram $bot)
+    public function start(Nutgram $bot)
     {
-        $bot->sendMessage('How big should be you ice cream cup?', [
-            'reply_markup' => InlineKeyboardMarkup::make()
-                ->addRow(InlineKeyboardButton::make('Small', callback_data: 'S'), InlineKeyboardButton::make('Medium', callback_data: 'M'))
-                ->addRow(InlineKeyboardButton::make('Big', callback_data: 'L'), InlineKeyboardButton::make('Super Big', callback_data: 'XL')),
-        ]);
-        $this->next('askFlavors');
+        $bot->sendMessage('This is the first step!');
+        $this->next('secondStep');
     }
 
-    public function askFlavors(Nutgram $bot)
+    public function secondStep(Nutgram $bot)
     {
-        // if is not a callback query, ask again!
-        if (!$bot->isCallbackQuery()) {
-            $this->askCupSize($bot);
-            return;
-        }
-
-        $this->cupSize = $bot->callbackQuery()->data;
-
-        $bot->sendMessage('What flavors do you like?');
-        $this->next('recap');
-    }
-
-    public function recap(Nutgram $bot)
-    {
-        $flavors = $bot->message()->text;
-        $bot->sendMessage("You want an $this->cupSize cup with this flavors: $flavors");
+        $bot->sendMessage('Bye!');
         $this->end();
     }
 }
