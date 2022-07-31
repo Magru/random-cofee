908b34d2002fa4df2c2d3e5b39a6b9c38f3adb15
Maxim Folko <maxf@leos.co.il>
2022-07-31 13:32:13 +0300

    bot tests

--- routes/telegram.php
+++ routes/telegram.php
@@ -1,6 +1,7 @@
 <?php
 /** @var SergiX44\Nutgram\Nutgram $bot */
 
+use SergiX44\Nutgram\Conversations\InlineMenu;
 use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
 
@@ -14,6 +15,36 @@ use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
 |
 */
 
+class ChooseColorMenu extends InlineMenu
+{
+
+    public function start(Nutgram $bot)
+    {
+        $this->menuText('Choose a color:')
+            ->addButtonRow(InlineKeyboardButton::make('Red', callback_data: 'red@handleColor'))
+            ->addButtonRow(InlineKeyboardButton::make('Green', callback_data: 'green@handleColor'))
+            ->addButtonRow(InlineKeyboardButton::make('Yellow', callback_data: 'yellow@handleColor'))
+            ->orNext('none')
+            ->showMenu();
+    }
+
+    public function handleColor(Nutgram $bot)
+    {
+        $color = $bot->callbackQuery()->data;
+        $this->menuText("Choosen: $color!")
+            ->showMenu();
+    }
+
+    public function none(Nutgram $bot)
+    {
+        $bot->sendMessage('Bye!');
+        $this->end();
+    }
+}
+
 $bot->onCommand('start', function (Nutgram $bot) {
-    return BotTestController::class;
+
+    $conv = new ChooseColorMenu();
+
+     return $conv->start();
 });
