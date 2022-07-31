6d3cc3f6658947731cc35b9f39e02a83f98028b2
Maxim Folko <maxf@leos.co.il>
2022-07-31 21:03:58 +0300

    registration command

--- routes/telegram.php
+++ routes/telegram.php
@@ -1,11 +1,7 @@
 <?php
 /** @var SergiX44\Nutgram\Nutgram $bot */
 
-use SergiX44\Nutgram\Conversations\Conversation;
-use SergiX44\Nutgram\Conversations\InlineMenu;
-use SergiX44\Nutgram\Nutgram;
-use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardButton;
-use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardMarkup;
+use App\Http\Telegram\TgRegistrationConversion;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,70 +15,4 @@ use SergiX44\Nutgram\Telegram\Types\Keyboard\InlineKeyboardMarkup;
 
 
 
-$bot->onCommand('example', AskIceCreamConversation::class);
-
-
-class ChooseColorMenu extends InlineMenu
-{
-
-    public function start(Nutgram $bot)
-    {
-        $this->menuText('Choose a color:')
-            ->addButtonRow(InlineKeyboardButton::make('Red', callback_data: 'red@handleColor'))
-            ->addButtonRow(InlineKeyboardButton::make('Green', callback_data: 'green@handleColor'))
-            ->addButtonRow(InlineKeyboardButton::make('Yellow', callback_data: 'yellow@handleColor'))
-            ->orNext('none')
-            ->showMenu();
-    }
-
-    public function handleColor(Nutgram $bot)
-    {
-        $color = $bot->callbackQuery()->data;
-        $this->menuText("Choosen: $color!")
-            ->showMenu();
-    }
-
-    public function none(Nutgram $bot)
-    {
-        $bot->sendMessage('Bye!');
-        $this->end();
-    }
-}
-
-class AskIceCreamConversation extends Conversation {
-
-    protected ?string $step = 'askCupSize';
-
-    public $cupSize;
-
-    public function askCupSize(Nutgram $bot)
-    {
-        $bot->sendMessage('How big should be you ice cream cup?', [
-            'reply_markup' => InlineKeyboardMarkup::make()
-                ->addRow(InlineKeyboardButton::make('Small', callback_data: 'S'), InlineKeyboardButton::make('Medium', callback_data: 'M'))
-                ->addRow(InlineKeyboardButton::make('Big', callback_data: 'L'), InlineKeyboardButton::make('Super Big', callback_data: 'XL')),
-        ]);
-        $this->next('askFlavors');
-    }
-
-    public function askFlavors(Nutgram $bot)
-    {
-        // if is not a callback query, ask again!
-        if (!$bot->isCallbackQuery()) {
-            $this->askCupSize($bot);
-            return;
-        }
-
-        $this->cupSize = $bot->callbackQuery()->data;
-
-        $bot->sendMessage('What flavors do you like?');
-        $this->next('recap');
-    }
-
-    public function recap(Nutgram $bot)
-    {
-        $flavors = $bot->message()->text;
-        $bot->sendMessage("You want an $this->cupSize cup with this flavors: $flavors");
-        $this->end();
-    }
-}
+$bot->onCommand('start', TgRegistrationConversion::class);
