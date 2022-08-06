169d218e081ea49ed55f0f3941514b93c5bb9f90
Maxim Folko <maxf@leos.co.il>
2022-08-06 18:08:48 +0300

    bot chat registration

--- app/Http/Telegram/TgRegistrationConversion.php
+++ app/Http/Telegram/TgRegistrationConversion.php
@@ -16,11 +16,6 @@ class TgRegistrationConversion extends Conversation {
     protected ?string $step = 'startConversation';
 
     public $_name;
-    public $_state;
-    public $_city;
-
-
-    public $cupSize;
 
     public function startConversation(Nutgram $bot)
     {
@@ -30,54 +25,14 @@ class TgRegistrationConversion extends Conversation {
         if($user){
             $bot->sendMessage('Chat ID:' . $id);
         }else{
-
             $bot->sendMessage('Давайте знакомиться. Как вас зовут?');
-            //$bot->sendMessage('Привет, ' . $this->_name . ':)');
             $this->next('askName');
         }
     }
 
     public function askName(Nutgram $bot){
         $this->_name = $bot->message()->text;
+        $bot->sendMessage('Привет, ' . $this->_name );
     }
 
-    public function askState(Nutgram $bot){
-
-        $states = State::all();
-
-
-        $bot->sendMessage('Из какого района вы? ', [
-            'reply_markup' => InlineKeyboardMarkup::make()
-                ->addRow(InlineKeyboardButton::make('Small', callback_data: 'S'), InlineKeyboardButton::make('Medium', callback_data: 'M'))
-                ->addRow(InlineKeyboardButton::make('Big', callback_data: 'L'), InlineKeyboardButton::make('Super Big', callback_data: 'XL')),
-        ]);
-        $this->next('askFlavors');
-    }
-
-    public function askCity(Nutgram $bot){
-
-    }
-
-
-
-    public function askFlavors(Nutgram $bot)
-    {
-        // if is not a callback query, ask again!
-        if (!$bot->isCallbackQuery()) {
-            $this->askCupSize($bot);
-            return;
-        }
-
-        $this->cupSize = $bot->callbackQuery()->data;
-
-        $bot->sendMessage('What flavors do you like?');
-        $this->next('recap');
-    }
-
-    public function recap(Nutgram $bot)
-    {
-        $flavors = $bot->message()->text;
-        $bot->sendMessage("You want an $this->cupSize cup with this flavors: $flavors");
-        $this->end();
-    }
 }
